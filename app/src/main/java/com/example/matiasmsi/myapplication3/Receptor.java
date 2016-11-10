ad72a684a7579ed57450f024252e8107a492e5d3
matias <m.flores.polanco@gmail.com>
2016-11-10 00:22:28 -0300

    iniciando receptor

---
+++ app/src/main/java/com/example/matiasmsi/myapplication3/Receptor.java
@@ -0,0 +1,42 @@
+package com.example.matiasmsi.myapplication3;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import org.ksoap2.serialization.SoapObject;
+
+/**
+ * Created by MatiasMSI on 09/11/2016.
+ */
+
+public class Receptor extends Activity {
+
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.formulario);
+    }
+
+    public  void EnviarOnclic(View v){
+
+        EditText 
+
+        Thread thread = new Thread(){
+            String res;
+            @Override
+            public void run(){
+                String NAMESPACE="http://tempuri.org/";
+                String URL= "http://ww2.qanalytics.cl/gps_migps/service.asmx";
+                String METHOD_NAME="WM_INS_REPORTE";
+                String SOAP_ACTION ="http://tempuri.org/WM_INS_REPORTE";
+
+                SoapObject request = new SoapObject(NAMESPACE,METHOD_NAME);
+                request.addProperty("",value)
+            }
+
+
+        };
+
+    }
+}
