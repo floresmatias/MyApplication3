f89b385d058b549e0e8ab394a36e3836a95c00f7
matias <m.flores.polanco@gmail.com>
2016-11-12 00:57:47 -0300

    iniciando receptor

--- app/src/main/java/com/example/matiasmsi/myapplication3/Receptor.java
+++ app/src/main/java/com/example/matiasmsi/myapplication3/Receptor.java
@@ -15,12 +15,12 @@ public class Receptor extends Activity {
 
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.formulario);
+        setContentView(R.layout.activity_vistaobrasoap);
     }
 
     public  void EnviarOnclic(View v){
 
-        EditText 
+
 
         Thread thread = new Thread(){
             String res;
@@ -32,7 +32,7 @@ public class Receptor extends Activity {
                 String SOAP_ACTION ="http://tempuri.org/WM_INS_REPORTE";
 
                 SoapObject request = new SoapObject(NAMESPACE,METHOD_NAME);
-                request.addProperty("",value)
+                //request.addProperty("",value)
             }
 
 
